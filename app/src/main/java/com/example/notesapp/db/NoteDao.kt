957d9264bc690cb9c0f7f1a55b94edf3a25eba12
Ahad <ahadsaad45@gmail.com>
2025-03-24 14:14:48 +0300

    add a Query to fetch individual notes by ID

--- app/src/main/java/com/example/notesapp/db/NoteDao.kt
+++ app/src/main/java/com/example/notesapp/db/NoteDao.kt
@@ -9,17 +9,24 @@ import androidx.room.Update
 
 @Dao
 interface NoteDao {
+
+    //Insert new notes into the database:
     @Insert
     suspend fun insert(noteModel: NoteModel)
 
+    //Update existing note content
     @Update
     suspend fun update(noteModel: NoteModel)
 
+    //Remove notes from the database
     @Delete
     suspend fun delete(noteModel: NoteModel)
 
+    //Query all notes
     @Query("SELECT * FROM notes")
     fun getAllNotes(): List<NoteModel>
 
-
+    //Query individual notes by ID
+    @Query("SELECT * FROM notes WHERE id = noteID")
+    suspend fun getNoteById(noteID: Int):NoteModel?
 }
